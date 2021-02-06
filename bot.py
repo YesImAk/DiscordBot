e53fb88a49f12042c353dc6dd8d71555647823d9
YesImAk <61521239+YesImAk@users.noreply.github.com>
2021-02-06 18:18:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,27 +86,26 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send("You dont have all the requirements :angry:")
 
-# THESE COMMANDS ARE DEACTIVATED
 
-#The below code bans player.
-#@client.command()
-#@commands.has_permissions(ban_members = True)
-#async def ban(ctx, member : discord.Member, *, reason = None):
-#   await member.ban(reason = reason)
+The below code bans player.
+@client.command()
+@commands.has_permissions(ban_members = True)
+async def ban(ctx, member : discord.Member, *, reason = None):
+   await member.ban(reason = reason)
 
-#The below code unbans player.
-#@client.command()
-#@commands.has_permissions(administrator = True)
-#async def unban(ctx, *, member):
-#    banned_users = await ctx.guild.bans()
-#    member_name, member_discriminator = member.split("#")
+The below code unbans player.
+@client.command()
+@commands.has_permissions(administrator = True)
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split("#")
 
-#   for ban_entry in banned_users:
-#        user = ban_entry.user
+   for ban_entry in banned_users:
+       user = ban_entry.user
 
-#        if (user.name, user.discriminator) == (member_name, member_discriminator):
-#            await ctx.guild.unban(user)
-#            await ctx.send(f'Unbanned {user.mention}')
-#            return
+       if (user.name, user.discriminator) == (member_name, member_discriminator):
+           await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
 
 client.run('')
